daa8874fb6e7553c2f428c04853029e61284fe66
imanishsharma <manisherma81@gmail.com>
2017-01-27 15:24:56 +0530

    its my first project

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/d/50d47b8ac3e200161433c5e873b2cd1e
@@ -0,0 +1,13 @@
+package com.spring.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloWorldController {
+	@RequestMapping("/home")
+public ModelAndView helloWorld(){
+	return new ModelAndView("welcome","message","Welcome to Spring Mvc framework");
+}
+}
---
+++ mvcdemo/src/main/java/com/spring/model/Person.java
@@ -0,0 +1,27 @@
+package com.spring.model;
+
+public class Person {
+	public String name;
+	public int mobile;
+	public String email;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getMobile() {
+		return mobile;
+	}
+	public void setMobile(int mobile) {
+		this.mobile = mobile;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	
+}
